4bf7fb3a42fe0772acff51ce500961cf372d7b86
Arban Hossain <arbanhossain@gmail.com>
2017-06-12 20:09:14 +0600

    updated-inspirona

---
+++ quotes.js
@@ -0,0 +1,167 @@
+var list = [
+			{
+				"quote":"Money cannot buy happiness. But it can buy you a lotta things. Those can give you happiness",
+				"author":"Someone"
+			},
+			{
+				"quote":"The best time to do your work was a year ago. The second best time is now",
+				"author":"Someone"
+			},
+			{
+				"quote":"Believe you can and you're halfway there",
+				"author":"Theodore Roosvelt"
+			},
+			{
+				"quote":"Do not take life too seriously. You will never get out of it alive",
+				"author":"Elbert Hubbard"
+			},
+			{
+				"quote":"Most people are other people. Their thoughts are someone else’s opinions, their lives a mimicry, their passions a quotation",
+				"author":"Oscar Wilde"
+			},
+			{
+				"quote":"People who think they know everything are a great annoyance to those of us who do",
+				"author":"Isaac Asimov"
+			},
+			{
+				"quote":"The truth does not change according to our ability to stomach it",
+				"author":"Flannery O'Connor"
+			},
+			{
+				"quote":"Those who do not move, do not notice their chains",
+				"author":"Rosa Luxemburg"
+			},
+			{
+				"quote":"Everyone thinks of changing the world but no one thinks of changing himself",
+				"author":"Leo Tolstoy"
+			},
+			{
+				"quote":"Don't let schooling interfere with your education",
+				"author":"Mark Twain"
+			},
+			{
+				"quote":"Every man dies, but not every man truly lives",
+				"author":"William Wallace"
+			},
+			{
+				"quote":"Some cause happiness wherever they go, others whenever they go",
+				"author":"Oscar Wilde"
+			},
+			{
+				"quote":"Even if you're on the right track, you'll get run over if you just sit there",
+				"author":"Will Rogers"
+			},
+			{
+				"quote":"When I was 5 years old, my mother always told me that happiness was the key to life. When I went to school, they asked me what I wanted to be when I grew up. I wrote down ‘happy’. They told me I didn’t understand the assignment, and I told them they didn’t understand life",
+				"author":"John Lennon"
+			},
+			{
+				"quote":"Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid",
+				"author":"from The Rythm of Life by Matthew Kelly"
+			},
+			{
+				"quote":"Dalai Llama, when asked what surprised him most about humanity, said, Man. Because he sacrifices his health in order to make money. Then he sacrifices money to recuperate his health. And then he is so anxious about the future that he does not enjoy the present; the result being that he does not live in the present or the future; he lives as if he is never going to die, and then dies having never really lived",
+				"author":"from Dalai Llama"
+			},
+			{
+				"quote":"I used to think the worst thing in life was to end up all alone , it's not ... The worst thing in life is to end up with people who make you feel all alone",
+				"author":"Robin Williams as Lance Clayton in the movie World's Greatest Dad"
+			},
+			{
+				"quote":"Education is what remains after one has forgotten what one has learned in school",
+				"author":"Albert Einstein"
+			},
+			{
+				"quote":"If the path before you is clear, you're probably on someone else's",
+				"author":"Joseph Campbell"
+			},
+			{
+				"quote":"Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius and a lot of courage to move in the opposite direction",
+				"author":"E.F. Schumacher"
+			},
+			{
+				"quote":"Don't cry because it's over, smile because it happened",
+				"author":"Dr. Seuss"
+			},
+			{
+				"quote":"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind",
+				"author":"Bernard M. Baruch"
+			},
+			{
+				"quote":"You only live once, but if you do it right, once is enough",
+				"author":"Mae West"
+			},
+			{
+				"quote":"Be the change that you wish to see in the world",
+				"author":"Mahatma Gandhi"
+			},
+			{
+				"quote":"Consider how hard it is to change yourself and you'll understand what little chance you have in trying to change others",
+				"author":"Abraham Lincoln"
+			},
+			{
+				"quote":"People are lonely because they build walls instead of bridges",
+				"author":"Abraham Lincoln"
+			},
+			{
+				"quote":"To live is the rarest thing in the world. Most people exist, that is all",
+				"author":"Oscar Wilde"
+			},
+			{
+				"quote":"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment",
+				"author":"Ralph Waldo Emerson"
+			},
+			{
+				"quote":"If you're the most intelligent person in the room,you're in the wrong room",
+				"author":"James D Watson"
+			},
+			{
+				"quote":"Give me six hours to chop down a tree, and I will spend the first four sharpening the axe",
+				"author":"Abraham Lincoln"
+			},
+			{
+				"quote":"Knowledge is knowing that a tomato is a fruit. Wisdom is knowing that a tomato doesn't belong in a fruit salad",
+				"author":"Miles Kington"
+			},
+			{
+				"quote":"If you really want to rebel against your parents: outearn them, outlive them, and know more than they do",
+				"author":"Henry Rollins"
+			},
+			{
+				"quote":"Always Remember you are braver than you believe, stronger than you seem & smarter than you think",
+				"author":"Winnie the Pooh"
+			},
+			{
+				"quote":"Holding on to anger is like drinking poison and expecting the other person to die",
+				"author":"attributed to Buddha"
+			},
+			{
+				"quote":"There is nothing noble in being superior to your fellow man; true nobility is being superior to your former self",
+				"author":"Ernest Hemingway"
+			},
+			{
+				"quote":"A reader lives a thousand lives before he dies. The man who never reads lives only one",
+				"author":"George R.R. Martin"
+			},
+			{
+				"quote":"I really don't care about being the first, so long as it inspires someone else to be second",
+				"author":"Raha Moharrak"
+			},
+			{
+				"quote":"Instead of wondering when your next vacation is, you ought to set up a life you don't need to escape from",
+				"author":"Seth Godin"
+			},
+			{
+				"quote":"Spend your money on the things money can buy. Spend your time on the things money can't buy",
+				"author":"Haruki Murakami"
+			},
+			{
+				"quote":"The problem is not the problem, the problem is your attitude about the problem. Do you understand?",
+				"author":"Captain Jack Sparrow"
+			},
+			{
+				"quote":"The world will not be destroyed by those who do evil, but by those who watch them without doing anything",
+				"author":"Albert Einstein"
+			}
+			
+]
